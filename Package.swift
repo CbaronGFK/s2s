ddf28709ec955b5b7bdb13a5c0a9616e8627cc87
CbaronGFK <90179246+CbaronGFK@users.noreply.github.com>
2022-08-02 13:18:53 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
             name: "s2s_sdk_ios",
             dependencies: []),
         .binaryTarget(
-             name: "S2S-SDK",
+             name: "s2s_sdk_ios",
              url:
                "https://s3.eu-central-1.amazonaws.com/download.sensic.net/s2s/sdk/ios/testing/1/s2s_sdk_ios_1.zip",
              checksum: "98c8e208f6dc67a6cf625d4432b0911d66359d3944f61d523359275f394083f3"
