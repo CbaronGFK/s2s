1d8c41285846c07e73761e238143c4cb6e7017c5
CbaronGFK <90179246+CbaronGFK@users.noreply.github.com>
2022-08-02 13:19:56 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "S2S_SDK",
-    platforms: [.iOS(.v10)],
+    platforms: [.iOS(.v9)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -18,10 +18,7 @@ let package = Package(
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "s2s_sdk_ios",
-            dependencies: []),
+        // Targets can depend on other targets in this package, and on products in packages this package depends on
         .binaryTarget(
              name: "s2s_sdk_ios",
              url:
